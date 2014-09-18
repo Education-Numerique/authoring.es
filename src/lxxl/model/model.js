b3fb822239fb652b0ddca86cc0e9a39641180267
root <root@ns304653.ip-94-23-30.eu>
2014-09-18 18:07:01 +0200

    collection id update

--- src/lxxl/model/model.js
+++ src/lxxl/model/model.js
@@ -129,8 +129,8 @@ jsBoot.pack('LxxlLib.model', function (api) {
     });
 
     this.Collection = api.TypedMutable.bind({}, {
-        id:'uk',
-        title:'United Kingdom'
+        id:'es',
+        title:'Espanol'
     });
 
     var SubActivity = api.TypedMutable.bind({}, {
